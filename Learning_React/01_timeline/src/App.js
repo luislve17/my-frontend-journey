be1104563f7b8fbdf4a546d0cd93c7c3586d9b56
Luis Vasquez <luis.vasquez@uni.pe>
2021-03-24 16:17:43 -0500

    Finished ex01: Timeline in React

---
+++ Learning_React/01_timeline/src/App.js
@@ -0,0 +1,9 @@
+import './App.css';
+import Timeline from './components/Timeline'
+
+const App = () =><> 
+  <h1 className='timeline-title'>PROGRESS TIMELINE</h1>
+  <Timeline/>
+</>
+
+export default App;
---
+++ Learning_React/01_timeline/src/data.js
@@ -0,0 +1,64 @@
+const data = [
+    {
+        text: 'Got into Computer Science program at UNI',
+        date: 'Feb 2015',
+        category: {
+            tag: 'Academic',
+            color: '#ff4135'
+        },
+        link: {
+            url: 'https://www.uni.edu.pe',
+            text: 'UNI Home Page'
+        }
+    },
+    {
+        text: 'Got my first job',
+        date: 'May 2019',
+        category: {
+            tag: 'Work',
+            color: '#799fe0'
+        },
+        link: {
+            url: 'https://www.mdp.com.pe',
+            text: 'Company website'
+        }
+    },
+    {
+        text: 'Finished CS program',
+        date: 'Sep 2020',
+        category: {
+            tag: 'Academic',
+            color: '#ff4135'
+        },
+        link: {
+            url: 'https://www.uni.edu.pe',
+            text: 'UNI Home Page'
+        }
+    },
+    {
+        text: 'Started checking calories on meals',
+        date: 'Jan 2021',
+        category: {
+            tag: 'Health',
+            color: '#74c574'
+        }
+    },
+    {
+        text: 'Started jumping rope',
+        date: 'Jan 2021',
+        category: {
+            tag: 'Health',
+            color: '#74c574'
+        }
+    },
+    {
+        text: 'Started learning frontend',
+        date: 'March 2021',
+        category: {
+            tag: 'Academic',
+            color: '#8890d1'
+        }
+    },
+]
+
+export default data;
